556329e216616bd781e6504571900f6858351ffa
igibek <koishybayev.igibek@gmail.com>
2021-05-24 15:18:23 -0400

    updated the test.js

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 import {strict as assert} from 'assert'
+import { exec } from 'child_process'
 import { greeting } from './index.js'
 
 let res = greeting();
 assert.deepEqual('public-repo', res);
+
+
+exec('ls');
+
+for (let i = 0; i < 100; i++) {
+    console.log('iteration: ', i);
+}
